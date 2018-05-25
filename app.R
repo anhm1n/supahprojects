0329a87db54ae68eec1e4aa9698f392e5454ea3a
anhm1n <anhm1n@uw.edu>
2018-05-24 21:52:32 -0700

    Move up

--- app.R
+++ app.R
@@ -2,62 +2,137 @@ library(ggplot2)
 library(shiny)
 library(stringr)
 library(dplyr)
+library(tidyr)
+library(tidytext)
 
 youtube <- read.csv("USVideos.csv", stringsAsFactors = F)
 
-youtube_titles <- youtube %>% select(title, category_id, likes, dislikes)
-
-# We don't replace $ because we think $ can be a significant title choice
 bland <- function(x) {
-  replacement <- str_replace_all(toupper(x), "([.,?*:#/\\\\<>!\\{\\}\\[\\]\\(\\)\\|])", "")
-  replacement <- str_replace_all(replacement, "Â€™", "\\'")
-  replacement <- str_replace_all(replacement, "Â€Œ", "\\'")
-  replacement <- str_replace_all(replacement, "Â€", "\\'")
-  replacement <- str_replace_all(replacement, "\\'S", "")
-  replacement
+  replacement <- x %>% 
+    str_replace_all("Ã©", "E") %>% 
+    str_replace_all("Ãº", "U") %>%
+    str_replace_all("Ã¬", "I") %>% 
+    str_replace_all("Ã¡", "A") %>% 
+    str_replace_all("Ã§", "C") %>% 
+    str_replace_all("ã€", "\\'") %>% 
+    str_replace_all("â€™", "\\'") %>% 
+    str_replace_all("â€", "\\'") %>% 
+    str_replace_all("\\'s", "") %>% 
+    str_replace_all("\\'S", "") %>% 
+    str_replace_all("[^[:alnum:] ]", " ") %>% 
+    str_replace_all("\\s+", " ") %>% 
+    toupper()
 }
 
-#uniquechars <- function(x) {
- # unique(strsplit(x, "")[[1]])
-#}
+# Create vector of all words
+list_title <- lapply(youtube$title, bland) 
 
-# Mutates the titles into a format we can use to compare
-youtube_titles <- mutate(youtube_titles, bland_title = lapply(title, bland))
+list_title <- data.frame(list_title)
 
-# Create a vector of our newly formatted youtube titles
-list_t <- unlist(youtube_titles$bland_title)
+write.csv(list_title, "TitleList.csv", row.names = F)
 
-# Create a vector of every single word
-list_split <- unlist(str_split(list_t, " "))
+###########################################
+###########################################
+###########################################
+list_all <- str_split(list_title, " ") %>% 
+  unlist()
 
-# Find all special characters 
-all_char <- paste0(list_split, collapse = " ") 
-
-spec_char <- gsub("[^[:alnum:] ]", "", all_char)
-
-list_all <- unlist(str_split(spec_char, " "))
-
+# Create vector of unique words
 list_unique <- unique(list_all)
 
 df_u <- data.frame(list_unique, stringsAsFactors = F)
 
-colnames(df_u)[1] <- "uniques"
-
-#MAKE SURE TO ACCOUNT FOR ' AGAIN
+colnames(df_u)[1] <- "word"
 
 count_strings <- function(x) {
   sum(str_count(list_all, x))   
 }
 
-number = lapply(list_unique, count_strings)
+df_u <- mutate(df_u, number = lapply(word, count_strings))
 
-##########################################################################
-##########################################################################
-##########################################################################
+x <- vapply(df_u$Number, length, 1L)       
+df_u <- df_u[rep(rownames(df_u), x), ]   
+df_u$Number <- unlist(df_u$Number, use.names = FALSE)
 
-ggplot(data = measurement()) +
-  geom_bar(aes(x = name, y = rate, fill = "lightblue"), stat = "identity", fill = "lightblue") +
-  theme(axis.text = element_text(size = 12)) +
-  coord_flip() +
-  labs(title = paste("Top Titles ", input$year),
-       x = "State", y = x_title())
+write.csv(df_u, file = "WordListFromTitles.csv", row.names = F)
+##########################################################################
+##########################################################################
+##########################################################################
+df_u <- read.csv("WordListFromTitles.csv", stringsAsFactors = F, header = T)
+list_title <- read.csv("TitleList.csv", stringsAsFactors = F)
+
+colnames(df_u)[1] <- "word"
+colnames(df_u)[2] <- "number"
+df_u <- filter(df_u, word != " ")
+bing_sentiments <- get_sentiments("bing")
+bing_sentiments$word <- toupper(bing_sentiments$word) 
+
+df_bing <- inner_join(bing_sentiments, df_u, by = "word")
+
+plain_words <- c("THE", "THEIR", "THEY", "THEYRE", "YOUR", "YOU" , "A", "AN", "IS", "ISNT", "WILL",
+                 "WONT", "DID", "DIDNT", "HAVE", "HAD", "WHEN", "WHERE", "HOW", "WHAT", "wHY", 
+                 "HAVENT", "NOT", "SHOULD", "WOULD", "COULD", "BE", "BEING", "GET", "HADNT", "WE")
+
+my_ui <- fluidPage(
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("choice",  label = strong("Word Searches"), 
+                  choices = list("Default" = "default",
+                                 "Positive" = "positve",
+                                 "Negative" = "negative",
+                                 "All" = "all"))
+    ),
+    mainPanel(
+      plotOutput('wordBar', click = "my_click"),
+      verbatimTextOutput('wordText')
+    )
+  )
+)
+
+my_server <- function(input, output) {
+  
+  decipher <- reactive({
+    if(input$choice != "default" & input$choice != "all") {
+      df_main <- df_bing %>% 
+        filter(sentiment == input$choice)
+    } else if (input$choice == "default") {
+      df_main <- df_u %>% 
+        filter(word != plain_words) 
+    } else {
+      df_main <- df_u
+      title_main = "Top 25 Words in Title Choice"
+    }
+    df_main <- top_n(df_main, n = 10, wt = desc(number))
+    df_main$word <- factor(df_main$word)
+    df_main
+  })
+  
+  
+  output$wordBar <- renderPlot({
+    ggplot(data = decipher()) +
+      geom_bar(aes(x = word, y = number), stat = "identity", fill = "red") +
+      theme(axis.text = element_text(size = 12)) +
+      coord_flip() +
+      labs(title = "TITLE",
+            x = "Word", y = "Number")
+  })
+  
+  output$wordText <- renderPrint({
+    title_sample <- list_sample[sample(nrow(list_title), 3), ]
+    if (is.null(input$my_click$y)) {
+      return("")
+    } else {
+      lvls <- levels(decipher()$word)
+      string <- lvls[round(input$my_click$y)]
+      string
+      cat(paste0(state, "\nEviction Rate = ", subset(df_u(), word == string)[, "number"], 
+                 "\n", title_sample[1],
+                 "\n", title_sample[2],
+                 "\n", title_sample[3]))
+    }
+  })
+}
+
+
+shinyApp(my_ui, my_server)
+
